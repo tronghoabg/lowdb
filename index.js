102ed5fe8b9bb350a72244fa7a014d7b266a9aa8
tronghoabg <tronghoabg@gmail.com>
2020-06-03 14:54:32 +0700

    add feature delete

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ var FileSync = require('lowdb/adapters/FileSync');
 var adapter = new FileSync('db.json');
 var db = low(adapter);
 
+var shortid = require("shortid");
+
+
 // Set some defaults (required if your JSON file is empty)
 db.defaults({users: []})
   .write()
@@ -19,24 +22,51 @@ app.set("views", "./views");
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
-
+//router get site home
 app.get("/", (req, res) => {
   res.render('index')
 });
  
-app.get("/todos", function(req, res) {
+
+//router get site users
+app.get("/user", function(req, res) {
   var q = req.query.q;
-  var tempArr = db.get('users').value()
-  res.render("todos/index", {
-    todos: tempArr
+  if (q) {
+    var newArr = db.get('users').value()
+    console.log(newArr)
+    var listFilter = newArr.filter(function(user) {
+      return user.name.toUpperCase().indexOf(q.toUpperCase()) !== -1
+    })
+    res.render("user/index", {
+      user: listFilter
+    })
+  }
+  res.render("user/index", {
+    user: db.get('users').value()
   });
 });
-
-app.get("/todos/create", (req, res) => {
-  res.render("todos/create");
-});
  
-app.post("/todos/create", function(req, res) {
+
+//router get site create
+app.get("/user/create", (req, res) => {
+  res.render("user/create");
+});
+
+
+//routert post --- to add user
+app.post("/user/create", function(req, res) {
+  req.body.id = shortid.generate();
   db.get('users').push(req.body).write();
   res.redirect("back")
   });
+
+
+// Xoa user khoi data base
+
+app.get("/user/:id/delete", function(req, res) {
+  var id = req.params.id;
+  db.get('users')
+    .remove({id: id})
+      .write();
+  res.redirect("/user")
+})
